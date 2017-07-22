cce8f685f095dc4278ee189da9ec0e52b8b811aa
Junior Saldanha <30378044+umsaldanha@users.noreply.github.com>
2017-07-22 18:02:59 -0400

    Create 1132.c

---
+++ 1132.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+	int X, Y, i;
+
+	scanf("%d%d", &X, &Y);
+	if(X > Y)
+	{
+		int total1 = 0;
+		for (i = Y; i <= X; i++)
+		{
+			if(i % 13 != 0)
+				{
+					total1 += i;
+				}
+		}
+		printf("%d\n", total1);
+	}
+	else if(Y > X)
+	{
+		int total2 = 0;
+		for (i = X; i <= Y; i++)
+		{
+			if(i % 13 != 0)
+			{
+				total2 += i;
+			}
+		}
+		printf("%d\n", total2);
+	}
+	return 0;
+}
